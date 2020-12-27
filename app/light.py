1cb652e83ca922399744937388cf45e7cbf72271
Matthew Worthington <38858348+worthington10TW@users.noreply.github.com>
2020-12-27 20:33:55 +0000

    test

--- app/light.py
+++ app/light.py
@@ -14,7 +14,7 @@ class Light:
         self.pin = pin
 
     def on(self):
-        logging.debug(f'Light {self.pin} turning on...TEST')
+        logging.debug(f'Light {self.pin} turning on...TESTAgain')
         GPIO.output(self.pin, GPIO.HIGH)
         logging.debug(f'Light {self.pin} on')
 
