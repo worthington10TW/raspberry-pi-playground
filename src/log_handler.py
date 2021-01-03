cf9342c9001a575d886a8d12713784c82b2894db
Matthew Worthington <mworthing@thoughtworks.com>
2021-01-03 12:37:42 +0000

    Rotating log handler at 100mb

--- src/log_handler.py
+++ src/log_handler.py
@@ -9,7 +9,9 @@ def setup_logger(level):
     formatter = logging.Formatter(
         fmt='%(asctime)s %(levelname)-8s %(message)s',
         datefmt='%Y-%m-%d %H:%M:%S')
-    handler = handlers.RotatingFileHandler('logs/app.log', maxBytes=1e+8, backupCount=1)
+    handler = handlers.RotatingFileHandler('logs/app.log',
+                                           maxBytes=1e+8,
+                                           backupCount=1)
     handler.setFormatter(formatter)
     screen_handler = logging.StreamHandler(stream=sys.stdout)
     screen_handler.setFormatter(formatter)
